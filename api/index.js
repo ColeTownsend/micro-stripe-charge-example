14b19473065ad20af325a93abfb7d8fcc45f978e
Cole Townsend <cole@coletownsend.com>
2017-06-13 23:10:36 -0400

    logging?

--- api/index.js
+++ api/index.js
@@ -6,11 +6,9 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 module.exports = post(async (req, res) => {
   const data = await json(req);
 
-  console.log(data);
-
   stripe.charges.create(data, (err, resp) => {
     if (err) {
-      send(res, 400, {error: `Charge could not be created.`})
+      send(res, 400, {message: `Charge could not be created.`})
     }
     else {
       send(res, 200, {message: `Charge created.`})
